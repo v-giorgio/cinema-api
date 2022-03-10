3d25221ed8c9c7ce8325e8111e700aefac277b63
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-10 00:09:59 -0300

    feature: criação da MoviesController

---
+++ api/controllers/MoviesController.js
@@ -0,0 +1,63 @@
+const Movie = require("../models/Movie");
+
+class MoviesController {
+  static async getAllMovies(req, res) {
+    try {
+      const movies = await Movie.findAll();
+
+      return res.status(200).json(movies);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async getOneMovie(req, res) {
+    const { id } = req.params;
+    try {
+      const movie = await Movie.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(movie);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async createMovie(req, res) {
+    const movieData = req.body;
+    try {
+      const newMovie = await Movie.create(movieData);
+
+      return res.status(200).json(newMovie);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async updateMovie(req, res) {
+    const { id } = req.params;
+    const movieUpdate = req.body;
+    try {
+      await Movie.update(movieUpdate, { where: { id: Number(id) } });
+
+      const movieAfterUpdate = await Movie.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(movieAfterUpdate);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async deleteMovie(req, res) {
+    const { id } = req.params;
+    try {
+      await Movie.destroy({ where: { id: Number(id) } });
+      return res.status(200).json({ message: `Filme ${id} deletado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+}
+
+module.exports = MoviesController;
