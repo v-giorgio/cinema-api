97b9c6b28700219c368f397b9d01004df2eaf467
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-13 13:33:56 -0300

    feature: adicionar Procfile

--- api/index.js
+++ api/index.js
@@ -2,17 +2,20 @@ const express = require("express");
 require("dotenv").config({
   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
 });
+const cors = require("cors");
 const routes = require("./routes");
 
 require("./database");
 
 /* definir a porta para servir o app */
-const port = process.env.PORT;
+const port = process.env.PORT || 3007;
 
 const app = express();
 
 /* middleware: definir o padrão json para as rotas */
 app.use(express.json());
+
+app.use(cors());
 
 /* executar o arquivo de rotas na app */
 routes(app);
